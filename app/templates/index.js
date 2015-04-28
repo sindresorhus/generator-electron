13485cb5218b51091dbbc73af5bec3274af2c23a
Sindre Sorhus <sindresorhus@gmail.com>
2015-04-28 10:46:33 +0700

    es6ify

--- app/templates/index.js
+++ app/templates/index.js
@@ -1,11 +1,12 @@
 'use strict';
-var app = require('app');
-var BrowserWindow = require('browser-window');
+const app = require('app');
+const BrowserWindow = require('browser-window');
 
+// report crashes to the Electron project
 require('crash-reporter').start();
 
 // prevent window being GC'd
-var mainWindow = null;
+let mainWindow = null;
 
 app.on('window-all-closed', function () {
 	if (process.platform !== 'darwin') {
@@ -20,11 +21,11 @@ app.on('ready', function () {
 		resizable: false
 	});
 
-	mainWindow.loadUrl('file://' + __dirname + '/index.html');
+	mainWindow.loadUrl(`file://${__dirname}/index.html`);
 
 	mainWindow.on('closed', function () {
 		// deref the window
 		// for multiple windows store them in an array
 		mainWindow = null;
-	})
+	});
 });
