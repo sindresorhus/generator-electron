ae14fae67a9cef9c7d6015100d50c74f4e14fb1f
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-01 01:48:12 +0200

    remove uneeded `.bind()`

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ module.exports = yeoman.generators.Base.extend({
 			default: this.appname.replace(/\s/g, '-'),
 			filter: function (val) {
 				return _s.slugify(val);
-			}.bind(this)
+			}
 		}, {
 			name: 'githubUsername',
 			message: 'What is your GitHub username?',
