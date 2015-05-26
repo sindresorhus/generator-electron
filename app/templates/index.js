234be64395c3cfc53ed46904915351e7403adcf7
Mehdy Dara <mdara@eleven-labs.com>
2015-05-26 10:06:57 +0200

    fix(closeWindowsOsx): rebind listerner after mainWindow deref

--- app/templates/index.js
+++ app/templates/index.js
@@ -5,6 +5,24 @@ const BrowserWindow = require('browser-window');
 // report crashes to the Electron project
 require('crash-reporter').start();
 
+function createMainWindow () {
+	let browserWin = new BrowserWindow({
+		width: 600,
+		height: 400,
+		resizable: false
+	});
+
+	browserWin.loadUrl(`file://${__dirname}/index.html`);
+
+	return browserWin;
+}
+
+function onClosed () {
+	// deref the window
+	// for multiple windows store them in an array
+	mainWindow = null;
+}
+
 // prevent window being GC'd
 let mainWindow = null;
 
@@ -15,31 +33,15 @@ app.on('window-all-closed', function () {
 });
 
 app.on('activate-with-no-open-windows', function () {
+	console.log(mainWindow);
 	if (!mainWindow) {
 		mainWindow = createMainWindow();
+		mainWindow.on('closed', onClosed);
 	}
 });
 
 app.on('ready', function () {
 	mainWindow = createMainWindow();
 
-	mainWindow.on('closed', function () {
-		// deref the window
-		// for multiple windows store them in an array
-		mainWindow = null;
-	});
+	mainWindow.on('closed', onClosed);
 });
-
-function createMainWindow () {
-	var browserWin;
-
-	browserWin = new BrowserWindow({
-		width: 600,
-		height: 400,
-		resizable: false
-	});
-
-	browserWin.loadUrl(`file://${__dirname}/index.html`);
-
-	return browserWin;
-}
